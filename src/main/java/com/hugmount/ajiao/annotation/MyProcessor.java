460a93ef696caea2f4b272a70b277d6704bffc27
lihuiming <1469987440@qq.com>
2020-06-02 15:27:44 +0800

    添加备注

--- src/main/java/com/hugmount/ajiao/annotation/MyProcessor.java
+++ src/main/java/com/hugmount/ajiao/annotation/MyProcessor.java
@@ -22,21 +22,27 @@ import java.util.Set;
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @AutoService(Processor.class)
 public class MyProcessor extends AbstractProcessor {
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         final Context context = ((JavacProcessingEnvironment) processingEnv).getContext();
-        final JavacElements elementUtils = (JavacElements) processingEnv.getElementUtils();
         final TreeMaker treeMaker = TreeMaker.instance(context);
-        Set<? extends Element> elements = roundEnv.getRootElements();
+        final JavacElements elementUtils = (JavacElements) processingEnv.getElementUtils();
 
         for (Element element : roundEnv.getElementsAnnotatedWith(HelloWorld.class)) {
+            // 获取原文件class的所有方法
             JCTree.JCMethodDecl jcMethodDecl = (JCTree.JCMethodDecl) elementUtils.getTree(element);
-
-            treeMaker.pos = jcMethodDecl.pos;
+            // body即为方法体 利用treemaker的Block方法获取到一个新方法体将原来的替换掉。
+            // 第一个参数访问标志, 重点要关注的是第二个参数，也就是具体的方法体内容
+            // 它是一个List类型的参数，List里面每一个元素就代表一个语句块
             jcMethodDecl.body = treeMaker.Block(0, List.of(
+                    // 织入代码块用treeMaker.Exec实现
                     treeMaker.Exec(
+                            // Apply用于创建方法调用语法树节点参数:  typeargs：参数类型列表, fn：调用语句, args：参数列表
+                            // 这里面用到了两个方法，一个是treeMaker.Select（生成具体的方法），一个是treeMaker.Literal（方法的参数）
+                            // .treeMaker.Select里面套了很多层, 多级方法的第一级以treeMaker.Ident开始，然后一层套一层，直到整个方法结束。
                             treeMaker.Apply(
-                                    List.<JCTree.JCExpression>nil(),
+                                    List.nil(),
                                     treeMaker.Select(
                                             treeMaker.Select(
                                                     treeMaker.Ident(
@@ -46,11 +52,10 @@ public class MyProcessor extends AbstractProcessor {
                                             ),
                                             elementUtils.getName("println")
                                     ),
-                                    List.<JCTree.JCExpression>of(
-                                            treeMaker.Literal("Hello, world!!!")
-                                    )
+                                    List.of(treeMaker.Literal("Hello World "))
                             )
                     ),
+                    // 方法的原代码块
                     jcMethodDecl.body
             ));
         }
